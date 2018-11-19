b3f3f7dbf83228159930b539d504c9e18571da4c
Veronique SQ <fizzbones@gmail.com>
2018-11-19 16:03:01 +0000

    urls + checkout template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from accounts import urls as urls_accounts
 from products import urls as urls_products
 from search import urls as urls_search
 from cart import urls as urls_cart
+from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -27,6 +28,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^cart/', include(urls_cart)),
     url(r'^$', all_products, name='index'),
     url(r'^products/', include(urls_products)),
