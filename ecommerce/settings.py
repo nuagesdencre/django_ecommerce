5bf04ccf4ea63e2692e76f9d929a69c11ca651ec
Veronique SQ <fizzbones@gmail.com>
2018-11-21 15:23:57 +0000

    corretion typo settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,7 +143,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY =os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN= '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3BotoStorage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
