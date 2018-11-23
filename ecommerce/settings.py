de89662b3e162dd6e2f0b6e9146680b2776eaea0
Veronique SQ <fizzbones@gmail.com>
2018-11-23 14:51:51 +0000

    correction typo media file set up

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,9 +150,10 @@ STATICFILES_LOCATION = "static"
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 MEDIAFILES_LOCATION= 'media'
-DEFAUKT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
